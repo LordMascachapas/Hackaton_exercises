e7765c80c3ca0ca7501eda47d1addf2d70e6c124
LordMascachapas <alfredm3007@gmail.com>
2018-09-30 22:44:30 +0200

    Añadida función para destacar palabras
    Probar en los links del pie de página

---
+++ js/linkText.js
@@ -0,0 +1,9 @@
+function highlight(text) {
+	text.style.color = "blue";
+	text.style.textDecoration = "underline";
+}
+
+function conceal(text) {
+	text.style.color = "white";
+	text.style.textDecoration = "none";
+}
