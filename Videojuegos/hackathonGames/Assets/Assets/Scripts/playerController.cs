b3b04b3bf9831f845e380c6dbb7ed628aa2d5b61
LordMascachapas <alfredm3007@gmail.com>
2018-10-02 14:32:15 +0200

    Inicio de proyecto

---
+++ Videojuegos/hackathonGames/Assets/Assets/Scripts/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    Rigidbody2D rb;
+    public float speed;
+    public float rozamiento;
+    public float impulsoSalto;
+
+    public void Saltar()
+
+    public void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate(){
+        float movHorz = Input.GetAxis("Horizontal");
+        float salto = Input.GetAxis("Up");
+        Vector2 movement = new Vector2(movHorz, 0);
+	Vector2 vImpulso = new Vector2(0, salto);
+        rb.AddForce(movement * speed);
+        rb.AddForce(-rozamiento * rb.GetPointVelocity(this.transform.position));
+	rb.AddForce(vImpulso * impulsoSalto);
+    }
+
+}
