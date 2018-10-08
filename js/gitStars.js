1472bce6192ff186d6a12aba06e1a05ccc7473fd
LordMascachapas <alfredm3007@gmail.com>
2018-10-08 15:52:52 +0200

    ahora comprueba si se ha relizado la petición correctamente

--- js/gitStars.js
+++ js/gitStars.js
@@ -5,13 +5,21 @@ var Http = new XMLHttpRequest();
 var url='https://api.github.com/repos/LordMascachapas/Hackaton_exercises/stargazers';
 Http.open("GET", url);
 Http.send();
+
 Http.onreadystatechange=(e)=>{
-    data = JSON.parse(Http.responseText);
-    for(i = 0; i < descriptionTexts.length; i++){
-        if (data.length == 0) {
-            descriptionTexts[i].innerHTML = "¡¡¡ Este repositorio no tiene estrellas /(7_7)\\ !!!";
-        } else {
-            descriptionTexts[i].innerHTML = "Este repositorio tiene " + data.length + " estrellas :D";
-        }
-    }
+	console.log(e);
+	if (e.currentTarget.status == 200 && e.currentTarget.readyState == 4){
+	    data = JSON.parse(Http.responseText);
+	    for(i = 0; i < descriptionTexts.length; i++){
+	        if (data.length == 0) {
+	            descriptionTexts[i].innerHTML = "¡¡¡ Este repositorio no tiene estrellas /(7_7)\\ !!!";
+	        } else {
+	            descriptionTexts[i].innerHTML = "Este repositorio tiene " + data.length + " estrellas :D";
+	        }
+	    }
+	}else{
+		for(i = 0; i < descriptionTexts.length; i++){
+			descriptionTexts[i].innerHTML = "Error al conectar con la API de github";
+		}
+	}
 }
